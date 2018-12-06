edbf430d607d2f403cbf3ae8d244f47187c73a2f
MariekeDirk <marieke.dirksen@knmi.nl>
2018-12-06 15:06:40 +0100

    added cleaning function and IUTRECHT23

---
+++ R/CleanWunderground.R
@@ -0,0 +1,114 @@
+#'@title Clean and interpolate wunderground data
+#'@description Filters out non-realistic low values downloaded using `download_time_seq`, creates UTC timestamps
+#'from which a 10min sequence is created. The following filters are applied:
+#'
+#'\itemize{
+#'\item Temperatures lower than -40 are unrealistic in the Netherlands and are excluded (set to NA)
+#'\item Similarly temperatures above 45 are also excluded (set to NA)
+#'\item In case there are more than 8 identical measurements in a row the value is set to NA
+#'}
+#'
+#'If the gap between the measurements is more than 1 hour values
+#'are not interpolated.
+#'@param Tunit Temperature unit, since December 2018 the Wunderground data is in Farenheid.
+#'@param datafile Downloaded wunderground data
+#'@param fname Filename to write the filtered data
+#'@export
+clean_wunderground<-function(Tunit="Farenheid",
+                             datafile="C:/Users/marie/OneDrive/Documenten/uhi_max/inst/Wunderground/IUTRECHT23/daydata_rbind_2009-01-01_until_2018-11-27.txt",
+                             fname="C:/Users/marie/OneDrive/Documenten/uhi_max/inst/Wunderground/Filtered/IUTRECHT23_filtered.rds"){
+  requireNamespace("data.table",quietly = TRUE)
+  requireNamespace("lubridate",quietly = TRUE)
+
+  wur_stations_data<-fread(datafile)
+
+message("Preparing input data")
+  TC<-wur_stations_data[[1]] #select the temperatures
+  if(Tunit=="Farenheid"){TC<-(TC-32)/1.8}
+  wur_filtered<-ifelse(TC < -40 | TC > 45,NA,TC) #filter the temperature
+
+
+  #Also filter the duplicates in the data! If TRUE for more than 8 values in a row, remove the sequence
+  dub<-sequential.duplicated(wur_filtered)
+  DT <- data.table(dub, runid = rleid(dub))
+  DT_stat <- DT[,.(length = .N, position = .I[1], type=dub[1]), by = runid]
+  DT_to_filter<-DT_stat[which(as.numeric(DT_stat$length)>8 & DT_stat$type==TRUE),]
+  from<-DT_to_filter$position
+  to<-DT_to_filter$position+DT_to_filter$length-1
+  I_out<-unlist(mapply(seq,from,to,by=1,SIMPLIFY = TRUE))
+
+  wur_filtered[I_out]<-NA
+
+  TS<-wur_stations_data[[2]] #select the timestamps
+  ts_filtered<-as.POSIXct(TS,format="%Y-%m-%d %H:%M:%S")
+
+  df<-data.frame("T"=wur_filtered,"Timestamp_UTC"=ts_filtered)
+
+message("Find the gaps in the time series")
+    df_complete<-df[complete.cases(df),]
+    df_complete$gap_min<-c(as.numeric(diff(df_complete$Timestamp_UTC)/60),NA)
+    df_complete$over_tresh<-df_complete$gap_min>60
+
+    #Determine the length of the logical vector over_tresh
+    DT <- data.table(df_complete$over_tresh, runid = rleid(df_complete$over_tresh))
+    DT_stat <- DT[,.(length = .N, position = .I[1], type=df_complete$over_tresh[1]), by = runid]
+    # order by length
+    setorder(DT_stat, -length)
+    # Get top 3 (or n) by type
+    continuous_mm<-DT_stat[, .SD, type]
+    continuous_mm$days<-continuous_mm$length/(60*7)
+    continuous_mm$start<-df_complete$Timestamp_UTC[continuous_mm$position]
+    continuous_mm$stop<-df_complete$Timestamp_UTC[continuous_mm$position+(continuous_mm$length-1)]
+
+    Irel<-which(continuous_mm$days>7)
+    t_start<-round_date(continuous_mm$start[Irel],"10 mins")
+    t_stop<-round_date(continuous_mm$stop[Irel],"10 mins")
+    I_start<-continuous_mm$position[Irel]
+    I_stop<-continuous_mm$position+(continuous_mm$length-1)
+
+message("Approximating new values")
+    #Interpolating new values using a linear method
+    interpolate_period<-function(start,stop,I_start,I_stop){
+      t_sq<-seq(from=start,to=stop,by="10 mins")
+      T_new<-approx(x=df_complete$Timestamp_UTC[I_start:I_stop],
+                    y=df_complete$T[I_start:I_stop],xout=t_sq,
+                    rule=1,ties = "ordered",method="linear")
+      output<-data.frame(t_sq,T_new$y)
+      names(output)<-c("new_time","T_int")
+      return(output)
+    }
+
+    int_time<-mapply(interpolate_period,
+                     start=t_start,stop=t_stop,
+                     I_start=I_start,I_stop=I_stop,
+                     SIMPLIFY = FALSE)
+    df_int_time<-do.call("rbind",int_time)
+
+    output_ls<-list("filtered_obs"=df_complete,
+                    "continuous_measurements"=continuous_mm,
+                    "interpolated_time"=df_int_time)
+    saveRDS(output_ls,file = fname)
+  }
+
+#' Sequential duplicated from FedData package
+#' @title Get a logical vector of which elements in a vector are sequentially duplicated.
+#' @description Get a logical vector of which elements in a vector are sequentially duplicated.
+#' @param x An vector of any type, or, if rows, a matrix
+#' @param rows Is x a matrix?
+#' @return A logical vector of the same length as x.
+sequential.duplicated<-function (x, rows = F)
+{
+  if (!rows) {
+    duplicates <- c(FALSE, unlist(lapply(1:(length(x) - 1),
+                                         function(i) {
+                                           duplicated(x[i:(i + 1)])[2]
+                                         })))
+  }
+  else {
+    duplicates <- c(FALSE, unlist(lapply(1:(nrow(x) - 1),
+                                         function(i) {
+                                           duplicated(x[i:(i + 1), ])[2]
+                                         })))
+  }
+  return(duplicates)
+}
---
+++ R/DownloadWunderground.R
@@ -0,0 +1,96 @@
+#' Download Wunderground data
+#' @title Wunderground time series from URL
+#' @description Function to download 5min weather data from the \url{www.wunderground.nl} website. For a specific time period,
+#' station and variable(s) the data can be downloaded and saved in a directory. For each stationname a new folder is created (unless)
+#' the folder already exists. After downloading the daily files all the .txt files are combined and the daily files are deleted.
+#' @param stationname Character, name of the station
+#' @param start Date, start of the download period
+#' @param stop Date, stop period of the download period
+#' @param var Character, variable(s) which you want to download
+#' @param folder.loc Character, location were the data will be stored. For each stationname a new folder is created.
+#' @examples
+#' #Run the example below to store data at the current location
+#' download_time_seq(stationname = "IZUIDHOL87",
+#'                   start = as.Date("2018/04/01"),
+#'                   stop = as.Date("2018/05/01"),
+#'                   var = "TemperatureC",
+#'                   folder.loc = ".")
+#' For multiple stations use lapply:
+#' stations<-c('IZUIDHOL36','ILIMBURG36')
+#' lapply(stations,download_time_seq)
+#' @export
+download_time_seq<-function(stationname = "IUTRECHT23",
+                            start = as.Date("2009/01/01"),
+                            stop = as.Date("2018/11/27"),
+                            var = "TemperatureF",
+                            folder.loc = "C:/Users/marie/OneDrive/Documenten/uhi_max/inst/Wunderground/"){
+
+requireNamespace("lubridate")
+requireNamespace("data.table")
+
+if (dir.exists(paste0(folder.loc,"/",stationname))) {
+  message(paste0("Already a directory for available for ",stationname))
+  return(TRUE)
+} else {
+message(paste0("Creating a new directory for ",stationname))
+dir.create(paste0(folder.loc,"/",stationname)) # maakt nieuw mapje voor het station waar een textfile per dag in komt
+}
+
+time.seq<-seq(start,stop,by="day")
+
+
+for(i in 1:length(time.seq)){
+t<-time.seq[i]
+URL<-paste0("https://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=",stationname,"&day=",mday(t),"&year=",year(t),"&month=",month(t),"&format=1")
+
+df<-fread(URL,sep=",",fill=TRUE)
+
+#Main point of the complete.cases is to get rid of the <br> NA NA NA ... lines in the file
+
+df<-try(df[complete.cases(df[[var]])],TRUE) # In my case I'm only interested in the temperature
+try(write.table(df,paste0(folder.loc,"/",stationname,"/","daydata_",t,".txt"),row.names = FALSE,sep=","),TRUE)
+
+}
+
+txt.files=list.files(path=paste0(folder.loc,"/",stationname,"/"), full.names=TRUE, pattern=".txt")
+df<-lapply(txt.files,function(x)fread(x))
+
+#only select files with data
+rows<-unlist(lapply(df,nrow))
+I.sub<-which(rows!=0)
+
+if(length(I.sub)==0){
+  fld<-paste0(folder.loc,"/",stationname,"/")
+  unlink(fld,recursive=TRUE)
+  return(FALSE)
+}
+df<-df[I.sub]
+
+#check if the columns exist, rm non existing
+colnms<-unlist(lapply(df,function(x) "DateUTC<br>" %in% colnames(x) & var %in% colnames(x)))
+df<-df[colnms]
+
+if(length(df)==0){
+  fld<-paste0(folder.loc,"/",stationname,"/")
+  unlink(fld,recursive=TRUE)
+  return(FALSE)
+}
+
+df.t<-lapply(df,function(x)subset(x,select=c(var,"DateUTC<br>"))) #Subsetting Columns in all the txt files
+df.m<-Reduce(function(x,y) rbind(x,y,fill=TRUE),df.t)
+
+#exit if there is no data in the files
+if(nrow(df.m)==0){
+  fld<-paste0(folder.loc,"/",stationname,"/")
+  unlink(fld,recursive=TRUE)
+  return(FALSE)
+}
+names(df.m)<-c(var,"Timestamp_UTC")
+
+write.table(df.m,paste0(folder.loc,"/",stationname,"/","daydata_rbind_",start,"_until_",stop,".txt"),row.names = FALSE,sep=",")
+message(paste0("Combined all the files into: ","daydata_rbind_",start,"_until_",stop,".txt"))
+
+unlink(txt.files)   # haalt alle dagfiles weg, alleen combined_stationname.txt bestaat nog.
+message("removing all the single dayfiles")
+
+}
